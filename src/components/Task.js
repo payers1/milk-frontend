07c7ab25703f9572d8770611e2cd0e0574748167
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-10-31 21:12:55 -0400

    improve task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button, Card } from 'semantic-ui-react'
 
 const Task = (props) => {
   return (
     <div className="tasks">
-      <Button
-        content={props.nextTask.title}
-        onClick={props.nextTask.action} />
-      <div> <strong>Reward: </strong>1 Coin </div>
+      <style jsx>{`
+        .tasks {}
+      `}</style>
+      <Card>
+        <Card.Content>
+          <Card.Header> {props.nextTask.title} </Card.Header>
+          <Card.Meta> 1 COIN </Card.Meta>
+          <Card.Description> {props.nextTask.description} </Card.Description>
+        </Card.Content>
+         <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green' onClick={props.nextTask.action}> Approve</Button>
+        </div>
+      </Card.Content>
+      </Card>
     </div>
   )
 }
