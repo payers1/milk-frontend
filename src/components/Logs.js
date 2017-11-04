1a4421c038ddaebf31e946f957d8225a3951e1e1
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-11-03 20:20:30 -0400

    refactor logs component

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import { Label, Segment } from 'semantic-ui-react'
+import { getLogs } from '../api'
+const { lifecycle } = require('recompose');
+
+const withData = lifecycle({
+  state: { loading: true, logs: []},
+  async componentDidMount() {
+    const logs = await getLogs(this.props.contract.address);
+    this.setState({ logs, loading: false })
+  }
+})
 
 const withKeyFromProps = (Comp, propName) => (props) => <Comp key={props[propName]} {...props} />
 
-const LogMessage = ({user, message}) => (
-  <div> {`${new Date().toLocaleTimeString()}: ${user.first_name} ${message}`}</div>
+const LogMessage = ({user, message, created_at}) => (
+  <div> {`${new Date(created_at).toLocaleTimeString()}: ${user.first_name} ${message}`}</div>
 )
 
-const LogStyle = () => (
-  <style jsx>{`
-    .messages{
-      font-family: monospace;
-      text-transform: uppercase;
-    }
-  `}
- </style>
-)
+const LogMessageDisplay = withKeyFromProps(LogMessage, 'id');
 
-const LogMessageDisplay = withKeyFromProps(LogMessage, 'i');
-
-export const LogsAlt = ({logs}) => (
-  <Segment.Group>
+export const Logs = ({logs, loading}) => (
+  <Segment.Group >
     <Segment> Event Log <Label circular> {logs.length}</Label> </Segment>
-    <Segment secondary>
+    <Segment secondary >
       <div className='messages'>
-        <LogStyle />
+        <style jsx>{`
+          .messages{
+            font-family: monospace;
+            text-transform: uppercase;
+          }
+        `}
+       </style>
         {logs.map(LogMessageDisplay)}
       </div>
     </Segment>
   </Segment.Group>
 )
+
+export default withData(Logs)
