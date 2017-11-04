2868e323ce42178b57c523905da3814542f223f1
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-11-03 20:20:08 -0400

    separate table and form for exchanging

---
+++ src/components/ExchangeRateTable.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Table, Segment } from 'semantic-ui-react'
+import { getSellPrices } from '../api'
+const { lifecycle } = require('recompose');
+
+const withData = lifecycle({
+  state: { loading: true, exchangeRates: {USD: {}, LTC: {}, ETH: {}, BTC: {}} },
+  async componentDidMount() {
+    const prices = await getSellPrices();
+    this.setState({exchangeRates: {...prices}, loading: false})
+  }
+})
+
+const TableHeader = () => (
+  <Table.Header>
+    <Table.Row>
+    <Table.HeaderCell></Table.HeaderCell>
+    <Table.HeaderCell width={6}>BDC</Table.HeaderCell>
+    <Table.HeaderCell width={6}>USD</Table.HeaderCell>
+    </Table.Row>
+  </Table.Header>
+)
+
+const CurrencyRow = ({symbol, exchangeRate, price}) => (
+  <Table.Row>
+    <Table.Cell> {symbol} </Table.Cell>
+    <Table.Cell> {exchangeRate} </Table.Cell>
+    <Table.Cell> {price} </Table.Cell>
+  </Table.Row>
+)
+
+const ExchangeRateTable = (props) => (
+  <Segment basic loading={props.loading}>
+  <h1> EXCHANGE RATES </h1>
+    <Table celled>
+      <TableHeader />
+      <Table.Body>
+        {Object.keys(props.exchangeRates)
+               .map((currencySymbol) => (
+                 <CurrencyRow
+                    key={currencySymbol}
+                    exchangeRate={props.exchangeRates[currencySymbol].rate}
+                    price={props.exchangeRates[currencySymbol].amount}
+                    symbol={currencySymbol} />))}
+      </Table.Body>
+    </Table>
+  </Segment>
+)
+
+export default withData(ExchangeRateTable)
