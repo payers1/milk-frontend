e19c8dd4b9e4b81c04ecb0bb0d22e991bd099c58
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-11-12 19:32:58 -0500

    Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+
+export default class MenuExampleBasic extends Component {
+  state = {}
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu size='large'>
+        <Menu.Item header content='Blockchain Milk' />
+        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item position='right' content={this.props.user.name} />
+      </Menu>
+    )
+  }
+}
