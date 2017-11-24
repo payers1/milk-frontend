8472d699be055375df00f666cb6b74658c40febb
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-11-24 16:10:52 -0500

    refactor App to use withUport HOC

--- src/App.js
+++ src/App.js
@@ -1,64 +1,21 @@
-import React, { Component } from 'react'
-import getWeb3 from './utils/getWeb3'
-import uport from './utils/getUport'
-import { handleUportLogin } from './api'
+import React from 'react'
 import './App.css';
+import withUport from './withUport'
 import 'semantic-ui-css/semantic.min.css';
-import { withProps } from 'recompose'
 
 import Main from './Main';
 import { Nav } from './components'
 import { Container } from 'semantic-ui-react'
 
-const Loading = () => <h1>...</h1>
-const NotAuthorized = () => (<h1>Not Authed</h1>)
-
-class App extends Component {
-  state = {
-    loading: true,
-    account: null,
-    uport: {},
-    name: '',
-    authorized: false
-  }
-
-  componentDidMount() {
-    return getWeb3
-      .then(({web3}) => {
-        this.setState({ web3, uport })
-        return uport.requestCredentials({ requested: ['name', 'email'], notifications: true })
-      })
-      .then((uportCredentials) => handleUportLogin(uportCredentials))
-      .then(({ account_address: account, authorized, name }) => {
-        this.setState({ name, account, authorized, loading: false })
-      })
-  }
-
-  render() {
-    const {
-      authorized,
-      loading,
-      account,
-      uport,
-      name,
-      web3
-    } = this.state;
-
-    const MainWithProps = withProps({ account, uport, web3 })(Main)
-
-    const App = () => (
-      <div className='App'>
-        <Nav name={name} />
-        <Container>
-          <MainWithProps />
-        </Container>
-      </div>
-    )
-
-    if (loading) { return <Loading />}
-    else if (!authorized) { return <NotAuthorized /> }
-    else { return <App /> }
-  }
-}
-
-export default App
+const App = (props) => (
+  <div className='App'>
+    <Nav name={props.name} />
+    <Container>
+      <Main
+        uport={props.uport}
+        uPortAccount={props.uPortAccount}
+      />
+    </Container>
+  </div>
+)
+export default withUport(App)
